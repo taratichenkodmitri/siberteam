8642117442670eae0b84d1a0d865a742d57dbb68
taratichenkodmitri <tarataichenkodmitri@gmail.com>
2022-09-27 14:10:41 +0700

    feat: suits

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,7 @@ import org.example.poker.PokerHand;
 public class App {
     public static void main( String[] args ) {
         try {
-            PokerHand pokerHand = new PokerHand("KS 2H 5C JD TD");
+            PokerHand pokerHand = new PokerHand("KD 2H 5C JC TD");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
--- src/main/java/org/example/poker/PokerHand.java
+++ src/main/java/org/example/poker/PokerHand.java
@@ -5,19 +5,22 @@ import org.example.exception.IncorrectSuitException;
 
 import java.util.*;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class PokerHand {
     private final String patternHand = "[2-9TJQKA][SHDC] [2-9TJQKA][SHDC] [2-9TJQKA][SHDC] [2-9TJQKA][SHDC] [2-9TJQKA][SHDC]";
     private String hand;
 
     private List<Card> cards = new ArrayList<>();
+    private List<Suit> suits = new ArrayList<>();
 
     public PokerHand(String hand) throws IncorrectHandException, IncorrectSuitException {
         if(!Pattern.matches(this.patternHand, hand)) {
             throw new IncorrectHandException();
         }
         this.hand = hand;
-        initCards();
+        this.initCards();
+        this.initSuits();
     }
 
     private void initCards() throws IncorrectSuitException {
@@ -26,6 +29,11 @@ public class PokerHand {
            this.cards.add(new Card(card.charAt(0), card.charAt(1)));
         }
         Collections.sort(this.cards, Comparator.comparing(Card::getValue));
+    }
+
+    private void initSuits() {
+        this.suits = this.cards.stream().map(item -> item.suit).collect(Collectors.toList());
+        Collections.sort(this.suits);
     }
     public String getHand() {
         return hand;
